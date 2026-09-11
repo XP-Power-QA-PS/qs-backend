0e5abea172a0521e450e0eff859abee07a7bc91d
NgocHung11 <nhpel11@gmail.com>
2026-09-11 15:53:37 +0700

    refactor: remove unused imports in ComplaintTrackingNoGeneratorTest and CustomerComplaintController

--- src/main/java/pnh/dev/qs/complaint/controller/CustomerComplaintController.java
+++ src/main/java/pnh/dev/qs/complaint/controller/CustomerComplaintController.java
@@ -9,7 +9,6 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/test/java/pnh/dev/qs/complaint/service/ComplaintTrackingNoGeneratorTest.java
+++ src/test/java/pnh/dev/qs/complaint/service/ComplaintTrackingNoGeneratorTest.java
@@ -1,6 +1,5 @@
 package pnh.dev.qs.complaint.service;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,9 +7,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pnh.dev.qs.complaint.repository.CustomerComplaintRepository;
-
 import java.time.LocalDate;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
